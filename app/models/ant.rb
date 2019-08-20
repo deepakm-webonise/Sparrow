76f7d816e8b5b9deef91564e40628821a012a819
Deepak Mahakale <deepakmahakale@gmail.com>
2019-08-20 11:56:27 +0530

    Add http check

--- app/models/ant.rb
+++ app/models/ant.rb
@@ -3,7 +3,14 @@ class Ant < ActiveRecord::Base
   validates :in_url, presence: { message: 'Please type the valid url!' }
   # validates_format_of :in_url, with: URI::regexp
   after_create :generate_slug
+  before_create :check_http
   private
+
+  def check_http
+    unless in_url.starts_with?('http')
+      self.in_url = in_url.prepend('http://')
+    end
+  end
 
   def generate_slug
     self.update_attributes(slug: (id.to_s + (1+rand(8)).to_s).reverse.to_i.to_s(36))
