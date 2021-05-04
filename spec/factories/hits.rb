b734affa956fc87f25ade5b03d64e115341c2642
Deepak Mahakale <deepakmahakale@gmail.com>
2021-05-05 01:55:12 +0530

    Rubocop

--- spec/factories/hits.rb
+++ spec/factories/hits.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 FactoryGirl.define do
   factory :hit do
     latitude { 1.5 }
-longitude { 1.5 }
-country { "MyString" }
-ant { nil }
+    longitude { 1.5 }
+    country { 'MyString' }
+    ant { nil }
   end
-
 end
